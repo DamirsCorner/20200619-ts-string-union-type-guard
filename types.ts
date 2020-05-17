dcdcd5e2f39c401876e93e2884e57b9796a46b22
Damir Arh <damir.arh@gmail.com>
2020-05-17 12:35:37 +0200

    Add type guard

--- types.ts
+++ types.ts
@@ -7,13 +7,17 @@ export interface FilterValue {
 
 export const FILTER_KEYS: FilterKey[] = ['name', 'surname'];
 
+export function isFilterKey(key: string): key is FilterKey {
+  return FILTER_KEYS.includes(key as FilterKey);
+}
+
 export function parseFilter(queryParams: {
   [key: string]: string;
 }): FilterValue[] {
   const filter: FilterValue[] = [];
   for (const key in queryParams) {
-    if (FILTER_KEYS.includes(key as FilterKey)) {
-      filter.push({ key: key as FilterKey, value: queryParams[key] });
+    if (isFilterKey(key)) {
+      filter.push({ key, value: queryParams[key] });
     }
   }
   return filter;
